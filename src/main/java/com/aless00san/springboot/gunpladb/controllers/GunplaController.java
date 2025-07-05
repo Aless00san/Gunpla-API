f79d6831120d9e44f30e98c25e7e4dabfd45c26d
Aless00san <mumaitronzx@gmail.com>
2025-07-05 16:25:47 +0200

    Added CrossOrigin Support

--- src/main/java/com/aless00san/springboot/gunpladb/controllers/GunplaController.java
+++ src/main/java/com/aless00san/springboot/gunpladb/controllers/GunplaController.java
@@ -3,6 +3,7 @@ package com.aless00san.springboot.gunpladb.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +24,8 @@ public class GunplaController {
     @Autowired
     private IGunplaService gunplaService;
 
+    // CrossOrigin for allowing the front-end to acces the API
+    @CrossOrigin(origins = "http://localhost:5173")
     @GetMapping("/list")
     public List<Gunpla> listAllGunplas() {
         return gunplaService.findAll();
